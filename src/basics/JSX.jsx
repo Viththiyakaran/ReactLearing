c988f053a3bbe237884115439aa029e02769934b
Viththiyakaran <battikaran6@gmail.com>
2025-12-23 22:15:26 +0000

    Ternary logical

--- src/basics/JSX.jsx
+++ src/basics/JSX.jsx
@@ -1,16 +1,27 @@
-const isLogin = true;
-const Login = <h1>Welcome</h1>;
+//
+const isLogin = false;
+const Login = <h1>Welcome Viththiyakaran</h1>;
 
 const JSX = () => {
-  return (
-    <div>
-      {isLogin && (
-        <>
-          <p>Welcome</p> {username}
-        </>
-      )}
-    </div>
-  );
+  return <div>{isLogin ? Login : <span>Sorry</span>}</div>;
 };
 
 export default JSX;
+
+//Logical &&
+// const isLogin = true;
+// const Login = <h1>Welcome</h1>;
+
+// const JSX = () => {
+//   return (
+//     <div>
+//       {isLogin && (
+//         <>
+//           <p>Welcome</p> {username}
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default JSX;
