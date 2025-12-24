2bd732d7d0a8a9dceefc7c6d27180df369c68cf5
Viththiyakaran <battikaran6@gmail.com>
2025-12-24 12:20:32 +0000

    Add all

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import JSX from "./basics/JSX.jsx";
 import Conditional from "./rendering/Conditional.jsx";
+import Loading from "./rendering/Loading.jsx";
+import LoadingLangepage from "./practice/LoadingLandpage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
 
     {/* <JSX /> */}
-    <Conditional />
+    {/* <Conditional /> */}
+    {/* <Loading /> */}
+    <LoadingLangepage />
   </StrictMode>
 );
